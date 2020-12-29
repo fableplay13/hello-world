92144958f54df2b8e9caf20bc80a66d9826603ab
Giacomo Rossi <j.s.13@hotmail.it>
2020-12-29 21:34:56 +0100

    photo pwa

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { TestPhotoComponent } from './test-native/test-photo/test-photo.component';
-import { TestPhotoFncComponent } from './test-native/test-photo-fnc/test-photo-fnc.component';
 
 @NgModule({
     declarations: [
-        TestPhotoComponent,
-        TestPhotoFncComponent
+        TestPhotoComponent
     ],
-    entryComponents: [TestPhotoFncComponent],
+    entryComponents: [TestPhotoComponent],
     imports: [
         CommonModule
     ],
@@ -18,8 +16,7 @@ import { TestPhotoFncComponent } from './test-native/test-photo-fnc/test-photo-f
         Camera
     ],
     exports: [
-        TestPhotoComponent,
-        TestPhotoFncComponent
+        TestPhotoComponent
     ],
     schemas: [ 
         NO_ERRORS_SCHEMA,
--- src/app/shared/test-native/test-photo/test-photo.component.ts
+++ src/app/shared/test-native/test-photo/test-photo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { PhotoService } from '../../services/photo.service';
 
 @Component({
   selector: 'app-test-photo',
@@ -7,17 +7,9 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
   styleUrls: ['test-photo.component.scss']
 })
 export class TestPhotoComponent implements OnInit {
-  constructor(private camera: Camera) { }
-  options: CameraOptions = {
-    quality: 100,
-    destinationType: this.camera.DestinationType.FILE_URI,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE
-  }
-  ngOnInit() {
-    this.camera.getPicture(this.options).then((imageData) => {
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
-     }, (err) => {
-     });
+  constructor(public photoService: PhotoService) { }
+  ngOnInit() {}
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
   }
 }
